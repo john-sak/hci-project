8e4af9ac7104107debc54e01e83d6aa04af77d4a
JohnSak <79512710+john-sak@users.noreply.github.com>
2022-01-12 15:10:53 +0200

    started php

---
+++ php/sign-in-handler.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Sign-In Handler.php</title>
+  </head>
+  <body>
+    <?php
+      print "Hello from PHP file\n";
+      print "USERNAME is ".$_POST["user"]."\n";
+      print "PASSWORD is ".$_POST["pass"]."\n";
+    ?>
+  </body>
+</html>
