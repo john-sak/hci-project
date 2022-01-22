99d18dcca98c8f96de7490425fa8fc8707f2e479
JohnSak <79512710+john-sak@users.noreply.github.com>
2022-01-22 18:45:59 +0200

    update profile done

--- php/save-profile.php
+++ php/save-profile.php
@@ -53,9 +53,18 @@
     $_SESSION['gender'] = $gender;
   }
 
-  $bday = $_POST['bday'];
-  $bMonth = $_POST['bday'];
-  $bYear = $_POST['bday'];
+  if (isset($_POST['bday'])) {
+    $date = explode("-", $_POST['bday']);
+    $bDay = intval($date[2]);
+    $bMonth = intval($date[1]);
+    $bYear = intval($date[0]);
+    $query = "UPDATE users SET birthDay = $bDay, birthMonth = $bMonth, birthYear = $bYear WHERE ID = $id";
+    $result = $conn->query($query);
+    if (!$result) die($conn->error);
+    $_SESSION['bDay'] = $date[2];
+    $_SESSION['bMonth'] = $date[1];
+    $_SESSION['bYear'] = $date[0];
+  }
 
   if (isset($_POST['address']) && $_POST['address'] != '') {
     $address = $_POST['address'];
